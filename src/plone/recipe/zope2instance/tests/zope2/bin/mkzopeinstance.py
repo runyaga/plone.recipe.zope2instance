a0c598ae2e3a8c50f874ff4581715a4ad29b3e07
Andreas Jung <zopyxfilter@gmail.com>
2008-12-09 05:12:04 +0000

    respect existing directories
    svn path=/buildout/plone.recipe.zope2instance/branches/optional-user-option/; revision=77087

--- src/plone/recipe/zope2instance/tests/zope2/bin/mkzopeinstance.py
+++ src/plone/recipe/zope2instance/tests/zope2/bin/mkzopeinstance.py
@@ -3,10 +3,18 @@
 import sys
 import os
 
-dir_ = sys.argv[-3]
-os.mkdir(dir_)
-os.mkdir(os.path.join(dir_, 'etc'))
-os.mkdir(os.path.join(dir_, 'bin'))
+def _mkdir(d):
+    if not os.path.exists(d):
+        os.makedirs(d)
+
+try:
+    dir_ = sys.argv[-3]
+except:
+    dir_ = sys.argv[-1]
+
+_mkdir(dir_)
+_mkdir(os.path.join(dir_, 'etc'))
+_mkdir(os.path.join(dir_, 'bin'))
 
 for file_ in ('runzope', 'zopectl', 'runzope.bat', 'zopeservice.py'):
     f = open(os.path.join(dir_, 'bin', file_), 'w')
